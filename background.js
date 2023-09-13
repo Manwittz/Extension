05b6c79ff5ea277efc5c9e576a7b2eb99484d53b
[MASTER] roshii <Roshii@stibits.io>
2023-09-13 13:19:18 -0400

    Tab Management and Login Status Check

--- background.js
+++ background.js
@@ -1,3 +1,49 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-  // Your automation code here
+// Updated Background Script for new tasks
+chrome.runtime.onInstalled.addListener(() => {
+  // Websites and their login button selectors
+  const websites = {
+    "gmail": {
+        "url": "https://mail.google.com/",
+        "login_button_selector": "a[jsname='CwaK9']"
+    },
+    "youtube": {
+        "url": "https://www.youtube.com/",
+        "login_button_selector": "button[aria-label='Sign in']"
+    }
+};
+
+  // Check existing tabs
+  chrome.tabs.query({}, (tabs) => {
+    const openUrls = tabs.map(tab => tab.url);
+
+    for (const site in websites) {
+      const siteInfo = websites[site];
+      if (!openUrls.includes(siteInfo.url)) {
+        // Open the website in a new tab if not already open
+        chrome.tabs.create({ url: siteInfo.url });
+      }
+    }
+  });
+});
+
+// Listen to tab updates to perform login checks
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.status === 'complete') {
+    // Check if the website is in our list
+    for (const site in websites) {
+      const siteInfo = websites[site];
+      if (tab.url.startsWith(siteInfo.url)) {
+        // Inject content script to check login status
+        chrome.tabs.executeScript(tabId, {
+          code: \`
+            const loginButton = document.querySelector('${siteInfo['login_button_selector']}');
+            if (loginButton) {
+              // Perform click action to go to login or sign-up page
+              loginButton.click();
+            }
+          \`
+        });
+      }
+    }
+  }
 });
---
+++ content.js
@@ -0,0 +1,9 @@
+// Content Script for Data Scraping and Form Interaction
+console.log('Content script loaded');
+
+// Example: Scrape the page title
+const pageTitle = document.title;
+console.log('Page Title:', pageTitle);
+
+// Example: Fill out a form field (assuming an input field with id 'username')
+// document.getElementById('username').value = 'myUsername';
