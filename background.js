edc21b338d98fe2ded57e4f3293baadaca6c8460
[MASTER] roshii <Roshii@stibits.io>
2023-09-13 13:33:08 -0400

    fix errors: all_urls, and syntax

--- background.js
+++ background.js
@@ -1,7 +1,5 @@
-// Updated Background Script for new tasks
-chrome.runtime.onInstalled.addListener(() => {
-  // Websites and their login button selectors
-  const websites = {
+// Updated Background Script for new tasks, compatible with Manifest V3
+const websites = {
     "gmail": {
         "url": "https://mail.google.com/",
         "login_button_selector": "a[jsname='CwaK9']"
@@ -60,18 +58,17 @@ chrome.runtime.onInstalled.addListener(() => {
     }
 };
 
-  // Check existing tabs
-  chrome.tabs.query({}, (tabs) => {
-    const openUrls = tabs.map(tab => tab.url);
+// Check existing tabs
+chrome.tabs.query({}, (tabs) => {
+  const openUrls = tabs.map(tab => tab.url);
 
-    for (const site in websites) {
-      const siteInfo = websites[site];
-      if (!openUrls.some(url => url && url.startsWith(siteInfo.url))) {
-        // Open the website in a new tab if not already open
-        chrome.tabs.create({ url: siteInfo.url });
-      }
+  for (const site in websites) {
+    const siteInfo = websites[site];
+    if (!openUrls.some(url => url && url.startsWith(siteInfo.url))) {
+      // Open the website in a new tab if not already open
+      chrome.tabs.create({ url: siteInfo.url });
     }
-  });
+  }
 });
 
 // Listen to tab updates to perform login checks
@@ -82,14 +79,16 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
       const siteInfo = websites[site];
       if (tab.url.startsWith(siteInfo.url)) {
         // Inject content script to check login status
-        chrome.tabs.executeScript(tabId, {
-          code: \`
-            const loginButton = document.querySelector('${siteInfo['login_button_selector']}');
+        chrome.scripting.executeScript({
+          target: {tabId: tabId},
+          function: (siteInfo) => {
+            const loginButton = document.querySelector(siteInfo.login_button_selector);
             if (loginButton) {
               // Perform click action to go to login or sign-up page
               loginButton.click();
             }
-          \`
+          },
+          args: [siteInfo]
         });
       }
     }
